88d03a97f03376a6126fd344706e77e17c520ac1
Ryan Hantak <rhantak@gmail.com>
2020-01-13 02:16:58 -0700

    Add additional current weather data

--- app/poros/current_forecast.rb
+++ app/poros/current_forecast.rb
@@ -6,5 +6,9 @@ class CurrentForecast
     @date = Time.at(current_weather['time']).strftime("%m/%d")
     @temp = current_weather['temperature'].round
     @weather = current_weather['summary']
+    @feels_like = current_weather['apparentTemperature'].round
+    @humidity = "#{(current_weather['humidity']*100).round}%"
+    @visibility = "#{current_weather['visibility'].round(2)} miles"
+    @uv_index = current_weather['uvIndex']
   end
 end
