a4e44983af16b888ede45dd9fa4903e70a3cf7dd
Ryan Hantak <rhantak@gmail.com>
2020-01-13 10:03:09 -0700

    Add route for antipodes endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecasts#index'
+
+      get '/antipode', to: 'antipodes#index'
     end
   end
 end
