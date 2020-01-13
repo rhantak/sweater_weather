a170746424e0756fc828e597b2bd49949c7236b1
Ryan Hantak <rhantak@gmail.com>
2020-01-13 01:08:32 -0700

    Add test for current facade method

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -5,10 +5,22 @@ describe 'forecast facade' do
     json_response = File.read('spec/fixtures/denver_geocode_data.json')
     stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?key=#{ENV['GOOGLE_KEY']}&address=denver,co").
       to_return(status: 200, body: json_response)
+
+    darksky_response = File.read('spec/fixtures/denver_darksky_data.json')
+    stub_request(:get, "https://api.darksky.net/forecast/#{ENV['DARKSKY_KEY']}/39.7392358,-104.990251?exclude=minutely,flags").
+      to_return(status: 200, body: darksky_response)
+
+    @facade = ForecastFacade.new('denver,co')
   end
 
   it 'city' do
-    facade = ForecastFacade.new('denver,co')
-    expect(facade.city).to eq('Denver, CO, USA')
+    expect(@facade.city).to eq('Denver, CO, USA')
+  end
+
+  it 'current' do
+    expect(@facade.current.time).to eq("12:10AM")
+    expect(@facade.current.date).to eq("01/13")
+    expect(@facade.current.temp).to eq(26)
+    expect(@facade.current.weather).to eq("Clear")
   end
 end
