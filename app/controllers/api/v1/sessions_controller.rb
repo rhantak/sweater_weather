a5013fffe054126c6ebed4c90e61057287c220ab
Ryan Hantak <rhantak@gmail.com>
2020-01-14 15:58:55 -0700

    Add session controller with create action to log in a user

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user &. authenticate(params[:password])
+      render json: UserSerializer.new(user)
+    end
+  end
+end
