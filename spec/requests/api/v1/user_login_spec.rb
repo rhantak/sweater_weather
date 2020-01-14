142d7a7db98b8520358113d4f73f5c8e0821aa3f
Ryan Hantak <rhantak@gmail.com>
2020-01-14 15:58:24 -0700

    Add test for user login endpoint

---
+++ spec/requests/api/v1/user_login_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'weather api' do
+  it 'can log in a user' do
+    user = User.create( email: 'whatever@example.com',
+                        password: 'password',
+                        password_confirmation: 'password')
+
+    post '/api/v1/sessions',
+    params: {
+              "email": "whatever@example.com",
+              "password": "password"
+            }
+
+    user_info = JSON.parse(response.body)
+
+    expect(response).to have_http_status(200)
+    expect(user_info['data']['attributes']['api_key']).to eq(user.api_key)
+  end
+end
