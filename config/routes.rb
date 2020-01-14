f5f7ec6383a8efc02e71f9a4b3aeaac5cee78bc1
Ryan Hantak <rhantak@gmail.com>
2020-01-14 14:42:01 -0700

    Add route for background image endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get '/forecast', to: 'forecasts#index'
+      get '/forecast',    to: 'forecasts#index'
+      get '/backgrounds', to: 'backgrounds#index'
     end
   end
 end
