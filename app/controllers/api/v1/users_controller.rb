d8d2749cf89be4b377eb79969c62abf4c393f263
Ryan Hantak <rhantak@gmail.com>
2020-01-14 16:38:14 -0700

    Add error message for bad registration details

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::UsersController < ApplicationController
     user = User.new(user_params.merge(api_key: SecureRandom.hex(13)))
     if user.save
       render json: UserSerializer.new(user), status: 201
+    else
+      render json: error_message(user), status: 400
     end
   end
 
@@ -10,5 +12,14 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params
     params.permit(:email, :password, :password_confirmation)
+  end
+
+  def error_message(user)
+    {errors:
+      [{
+        "status": 400,
+        "detail": "#{user.errors.full_messages.to_sentence}"
+        }]
+    }
   end
 end
