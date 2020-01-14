fca9046fe44618ab692b5aeed801e7db4bad4e0d
Ryan Hantak <rhantak@gmail.com>
2020-01-14 16:24:06 -0700

    Add conditional render for bad credentials error

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,19 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user &. authenticate(params[:password])
-      render json: UserSerializer.new(user)
+      render json: UserSerializer.new(user), status: 200
+    else
+      render json: bad_credentials, status: 400
     end
+  end
+
+  private
+
+  def bad_credentials
+    {errors:
+    [{
+      "status": 400,
+      "title": "Bad Credentials"
+    }]}
   end
 end
