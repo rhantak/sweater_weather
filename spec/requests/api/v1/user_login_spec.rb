5b473d7f205bb15496f4d63766a1eeaf0d1c7ad0
Ryan Hantak <rhantak@gmail.com>
2020-01-14 16:23:30 -0700

    Add test for bad credentials in login endpoint

--- spec/requests/api/v1/user_login_spec.rb
+++ spec/requests/api/v1/user_login_spec.rb
@@ -17,4 +17,18 @@ describe 'weather api' do
     expect(response).to have_http_status(200)
     expect(user_info['data']['attributes']['api_key']).to eq(user.api_key)
   end
+
+  it 'sends an error message for bad credentials' do
+    post '/api/v1/sessions',
+    params: {
+              "email": "whatever@example.com",
+              "password": "password"
+            }
+
+    user_info = JSON.parse(response.body)
+
+    expect(response).to have_http_status(400)
+
+    expect(user_info['errors'].first['title']).to eq("Bad Credentials")
+  end
 end
